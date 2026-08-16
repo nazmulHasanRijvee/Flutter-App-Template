a28f11ee9de177124049e3e50ee9cb556a724301
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-08-17 03:48:59 +0600

    Added a flutter template for ease of setup

---
+++ lib/app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_template/core/providers/theme_provider.dart';
+import 'package:flutter_app_template/core/routes/part_of.dart';
+import 'package:flutter_app_template/core/static/theme/theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    final themeMode = ref.watch(themeProvider).value;
+
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      ensureScreenSize: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          title: 'Flutter Template',
+          debugShowCheckedModeBanner: false,
+          theme: context.lightTheme,
+          darkTheme: context.darkTheme,
+          themeMode: themeMode,
+
+          /// ThemeMode.system
+          routerConfig: router,
+        );
+      },
+    );
+  }
+}
