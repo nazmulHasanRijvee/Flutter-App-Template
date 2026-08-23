eff78f519caf72f7639c662e9322ee8e237773e7
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-08-23 16:33:55 +0600

    update : added more design tokens

--- lib/core/static/theme/src/theme_extensions/src/dimensions.dart
+++ lib/core/static/theme/src/theme_extensions/src/dimensions.dart
@@ -41,6 +41,9 @@ class AppSpacing {
   // extra small, 4 px
   final double xs = 4;
 
+  /// small 2, 6 px
+  final double sm2 = 6;
+
   // small, 8 px
   final double sm = 8;
 
@@ -50,11 +53,17 @@ class AppSpacing {
   // large, 16 px
   final double lg = 16;
 
+  /// 20 px
+  final double lg2 = 20;
+
   // extra large, 24 px
   final double xl = 24;
 
   // extra extra large, 32 px
   final double xxl = 32;
+
+  /// extra extra large2, 40 px
+  final double xxl2 = 40;
 
   // extra extra extra large, 48 px
   final double xxxl = 48;
@@ -66,6 +75,9 @@ class AppSpacing {
 /// Border Radius design tokens
 class AppRadius {
   const AppRadius();
+
+   /// extra extra small, 2 px
+  final double xxs = 2;
 
   /// extra small, 4 px
   final double xs = 4;
@@ -99,8 +111,14 @@ class AppSizes {
   /// medium, 32 px
   final double md = 32;
 
+   /// medium 2, 40 px
+  final double md2 = 40;
+
   /// large, 48 px
   final double lg = 48;
+
+   /// large 2, 56 px
+  final double lg2 = 56;
 
   /// extra large, 64 px
   final double xl = 64;
